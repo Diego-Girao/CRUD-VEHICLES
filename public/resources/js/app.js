69c6487712e18f31ef2058ef1a02a4af00c89259
Diego-Girao <diegoramiro25@gmail.com>
2021-06-07 20:44:20 -0300

    apenas verificação, nada alterado.

--- public/resources/js/app.js
+++ public/resources/js/app.js
@@ -2,5 +2,6 @@ const repository = new VehicleRepository();
 const viewVehicles = new ViewVehicles(repository);
 const viewFormVehicles = new FormVehicles(repository, viewVehicles);
 
+
 viewVehicles.render();
 viewFormVehicles.render();
